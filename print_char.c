4f1c22388b819743577371669f25e4dddf9760b2
benji3106 <benjamin3106@live.fr>
2025-11-24 15:53:43 +0100

    feat: implement print_char function to print a character

---
+++ print_char.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_char - prints a character
+ * @args: list of arguments
+ *
+ * Return: number of characters printed
+ */
+int print_char(va_list args)
+{
+	char c = (char)va_arg(args, int);
+
+	_putchar(c);
+	return (1);
+}
