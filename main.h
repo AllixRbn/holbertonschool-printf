585babdeb3a92b869b4db0f326b58536fa7570a9
benji3106 <benjamin3106@live.fr>
2025-11-24 17:39:26 +0100

    feat: add choose_function declaration to main.h for dynamic function selection

--- main.h
+++ main.h
@@ -7,7 +7,9 @@ int _putchar(char c);
 int print_char(va_list args);
 int print_string (va_list str);
 int print_percentage(va_list args);
+int choose_function(char spec, va_list args);
 int _printf(const char *format, ...);
+
 
 /** 
 * struct printer : struct for choosing the right print function.
