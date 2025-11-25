f45d4cf0069809382afe6eecc42b4cfc704eb552
benji3106 <benjamin3106@live.fr>
2025-11-25 14:14:21 +0100

    feat: implement _printf function to print an output thanks to different formats

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * _printf - Produces output according to a format.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0;
+	int count = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format[i] != NULL)
+	{
+		if (format[i] == '%')
+		{
+			count++;
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+
+		if (format[i] == '\0')
+		{
+			break;
+		}
+
+		count += handle_format(format[i], args);
+		{
+		i++;
+		}
+	}
+
+	va_end(args);
+
+	return (count);
+}
